059dd843848d5f9d0377d87928792a88f8352282
afsalahamed07 <afsalahamed07@gmail.com>
2024-04-18 16:02:14 +0530

    Removing unused jackson serialization from Feed

--- src/main/java/org/araa/domain/Feed.java
+++ src/main/java/org/araa/domain/Feed.java
@@ -1,19 +1,12 @@
 package org.araa.domain;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.FeedException;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jdom2.Document;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
 
 @Data
@@ -41,17 +34,4 @@ public class Feed {
         this.link = syndFeed.getLink();
         this.entries = syndFeed.getEntries().stream().map(Entry::buildEntry).toList();
     }
-
-    // Serialize this Feed object to a JSON string
-    public static String serializeToJson(Feed feed) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(feed);
-    }
-
-    // Deserialize a Feed object from a JSON string
-    public static Feed deserializeFromJson(String json) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(json, Feed.class);
-    }
-
 }
