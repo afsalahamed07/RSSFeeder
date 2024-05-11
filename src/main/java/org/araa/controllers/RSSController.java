42ba097d0d5aec661b8a410fa55ad407265158db
afsalahamed07 <afsalahamed07@gmail.com>
2024-05-11 18:50:36 +0530

    removing unused imports

--- src/main/java/org/araa/controllers/RSSController.java
+++ src/main/java/org/araa/controllers/RSSController.java
@@ -3,14 +3,10 @@ package org.araa.controllers;
 import com.rometools.rome.io.FeedException;
 import lombok.AllArgsConstructor;
 import org.araa.application.dto.RSSDto;
-import org.araa.domain.RSS;
 import org.araa.services.RSSService;
 import org.hibernate.FetchNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
