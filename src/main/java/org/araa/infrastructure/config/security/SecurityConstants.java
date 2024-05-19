8c91136edfe15e2a165c5fba3729c9984797dbf6
afsalahamed07 <afsalahamed07@gmail.com>
2024-05-19 15:26:05 +0530

    increasing the expiration limit of jwt token

--- src/main/java/org/araa/infrastructure/config/security/SecurityConstants.java
+++ src/main/java/org/araa/infrastructure/config/security/SecurityConstants.java
@@ -6,6 +6,6 @@ public class SecurityConstants {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final long JWT_EXPIRATION_TIME = 1_000_000; // 15 minutes
+    public static final long JWT_EXPIRATION_TIME = 24_000_000; // 1 day
     public static final String JWT_SECRET = "secret_big_enough_for_production_security_1234567890_hopefully";
 }
