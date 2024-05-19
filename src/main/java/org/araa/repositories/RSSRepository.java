5f355a313f3c22445286d3d829c239034e1bfe17
afsalahamed07 <afsalahamed07@gmail.com>
2024-05-19 15:29:42 +0530

    removing redundant override from repo

--- src/main/java/org/araa/repositories/RSSRepository.java
+++ src/main/java/org/araa/repositories/RSSRepository.java
@@ -4,13 +4,9 @@ import org.araa.domain.RSS;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RSSRepository extends JpaRepository<RSS, Long> {
     boolean existsByUrl(String url);
 
     RSS findByUrl( String url );
-
-    List<RSS> findAll();
 }
