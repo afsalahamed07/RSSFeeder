df2f1022f861e8c1fd524a550887218f20d12424
afsalahamed07 <afsalahamed07@gmail.com>
2024-05-20 19:46:16 +0530

    separation of user detail extraction from rss service to auth service. non-blocking user subscription to rss.

--- src/main/java/org/araa/controllers/RSSController.java
+++ src/main/java/org/araa/controllers/RSSController.java
@@ -2,27 +2,48 @@ package org.araa.controllers;
 
 import com.rometools.rome.io.FeedException;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.araa.application.dto.RSSDto;
+import org.araa.domain.RSS;
+import org.araa.services.AuthService;
 import org.araa.services.RSSService;
+import org.araa.services.UserService;
 import org.hibernate.FetchNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping( "/api/v2/rss" )
 public class RSSController {
 
+    private static final Logger logger = LogManager.getLogger( RSSController.class );
+
     private final RSSService rssService;
+    private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping()
     public ResponseEntity<RSSDto> registerRSS( @RequestParam String url ) {
-        try {
-            RSSDto rssDto = new RSSDto( rssService.registerRSS( url ) );
-            return ResponseEntity.ok( rssDto );
+        UserDetails userDetails = authService.getAuthenticatedUser();
 
+        try {
+            RSS rss = rssService.registerRSS( url );
+
+            CompletableFuture.runAsync( () -> {
+                try {
+                    userService.subscribeRSS( userDetails.getUsername(), rss );
+                } catch ( FetchNotFoundException e ) {
+                    logger.info( "Failed to subscribe RSS for user {}", userDetails.getUsername() );
+                }
+            } );
+
+            return ResponseEntity.ok( new RSSDto( rss ) );
         } catch ( FeedException e ) {
             return ResponseEntity.badRequest().build();
         }
@@ -39,7 +60,7 @@ public class RSSController {
         }
     }
 
-    @GetMapping("/all")
+    @GetMapping( "/all" )
     public ResponseEntity<List<RSSDto>> fetchAllRSS() {
         List<RSSDto> rssDtos = rssService.getAllRSS();
         return ResponseEntity.ok( rssDtos );
--- src/main/java/org/araa/services/RSSService.java
+++ src/main/java/org/araa/services/RSSService.java
@@ -12,9 +12,6 @@ import org.araa.domain.RSS;
 import org.araa.infrastructure.utility.XMLParser;
 import org.araa.repositories.RSSRepository;
 import org.hibernate.FetchNotFoundException;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -27,33 +24,22 @@ public class RSSService {
     private static final Logger logger = LogManager.getLogger( RSSService.class );
 
     private final RSSRepository rssRepository;
-    private final UserService userService;
 
 
     public RSS registerRSS( String url ) throws FeedException {
         RSS rss;
-
-        if ( SecurityContextHolder.getContext().getAuthentication() == null ) {
-            logger.error( "User not authenticated" );
-            throw new AuthenticationException( "User not authenticated" ) {
-            };
-        }
-
-        UserDetails userDetails = ( UserDetails ) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
         if ( rssRepository.existsByUrl( url ) ) {
+            logger.info( "RSS already exists for {}", url );
             rss = rssRepository.findByUrl( url );
-            userService.subscribeRSS( userDetails.getUsername(), rss );
         } else {
             try {
                 SyndFeed syndFeed = XMLParser.parse( url );
                 rss = createRSSfromSyndFeed( syndFeed, url );
                 rss = rssRepository.save( rss );
-                userService.subscribeRSS( userDetails.getUsername(), rss );
+                logger.info( "RSS created for {}", url );
             } catch ( FeedException e ) {
                 throw new ParsingFeedException( "Failed to parse and create feed", e );
             }
-
         }
         return rss;
     }
