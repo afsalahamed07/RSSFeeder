49d434736f4b956383ad079055d6288f03f6eb91
afsalahamed07 <afsalahamed07@gmail.com>
2024-05-20 19:43:02 +0530

    changing the repositories to reflect the type change in the pk

--- src/main/java/org/araa/repositories/EntryRepository.java
+++ src/main/java/org/araa/repositories/EntryRepository.java
@@ -4,8 +4,10 @@ import org.araa.domain.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface EntryRepository extends JpaRepository<Entry, Long> {
+public interface EntryRepository extends JpaRepository<Entry, UUID> {
     Entry findByLink(String url);
 
     boolean existsByLink(String url);
--- src/main/java/org/araa/repositories/RSSRepository.java
+++ src/main/java/org/araa/repositories/RSSRepository.java
@@ -4,9 +4,11 @@ import org.araa.domain.RSS;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface RSSRepository extends JpaRepository<RSS, Long> {
-    boolean existsByUrl(String url);
+public interface RSSRepository extends JpaRepository<RSS, UUID> {
+    boolean existsByUrl( String url );
 
     RSS findByUrl( String url );
 }
