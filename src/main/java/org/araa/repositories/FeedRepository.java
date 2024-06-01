2a9c019c3e971423dfdb46efcb29f55798f1c007
afsalahamed07 <afsalahamed07@gmail.com>
2024-06-01 23:22:46 +0530

    removing individual feed logics

--- src/main/java/org/araa/repositories/FeedRepository.java
+++
@@ -1,8 +0,0 @@
-package org.araa.repositories;
-
-import com.rometools.rome.io.FeedException;
-import org.araa.application.dto.Feed;
-
-public interface FeedRepository {
-    Feed getFeed( String rss ) throws FeedException;
-}
