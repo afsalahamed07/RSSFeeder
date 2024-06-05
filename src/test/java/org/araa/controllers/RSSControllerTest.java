e305aaaee6ea2c911c444783eb3a524a092e9b1e
afsalahamed07 <afsalahamed07@gmail.com>
2024-06-05 19:17:58 +0530

    history save before refactoring code base

--- src/test/java/org/araa/controllers/RSSControllerTest.java
+++ src/test/java/org/araa/controllers/RSSControllerTest.java
@@ -1,22 +1,11 @@
 package org.araa.controllers;
 
-import org.araa.application.builder.FeedFactory;
-import org.araa.application.dto.Feed;
-import org.araa.application.dto.RSSDto;
-import org.araa.infrastructure.utility.XMLParser;
-import org.araa.services.FeedService;
 import org.araa.services.RSSService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
-
-import java.util.concurrent.CompletableFuture;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith( MockitoExtension.class)
 class RSSControllerTest {
