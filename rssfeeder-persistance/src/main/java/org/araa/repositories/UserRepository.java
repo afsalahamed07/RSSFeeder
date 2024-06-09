8f0537ce79f0e9d535c710eb64c09e7df8da95cb
afsalahamed07 <afsalahamed07@gmail.com>
2024-06-09 23:30:43 +0530

    removing unused queries

--- rssfeeder-persistance/src/main/java/org/araa/repositories/UserRepository.java
+++ rssfeeder-persistance/src/main/java/org/araa/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package org.araa.repositories;
 
 import org.araa.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,14 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByUsername( String username );
 
-    @Modifying
-    @Query( value = "INSERT INTO user_entry (user_id, entry_id) VALUES (:userId, :entryId)", nativeQuery = true )
-    void addEntryToUser( @Param( "userId" ) UUID userId, @Param( "entryId" ) UUID entryId );
-
-    @Modifying
-    @Query( value = "INSERT INTO user_subscriptions (user_id, rss_id) VALUES (:userId, :rssId)", nativeQuery = true )
-    void subscribeToRss( @Param( "userId" ) UUID userId, @Param( "rssId" ) UUID rssId );
-
-    @Query( value = "SELECT EXISTS(SELECT 1 FROM user_entry WHERE user_id = :userId AND entry_id = :entryId)", nativeQuery = true )
+    @Query( value = "SELECT EXISTS(SELECT 1 FROM user_entry WHERE user_id = :userId AND entry_id = :entryId)",
+            nativeQuery = true )
     boolean existsByEntry( @Param( "userId" ) UUID userId, @Param( "entryId" ) UUID entryId );
 }
